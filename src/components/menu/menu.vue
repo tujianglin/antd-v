304e699d2bf673c535e570fd375bf6610d971c99
涂江林 <jianglin.tu@wimisoft.com>
2025-10-21 19:30:46 +0800

    feat: Menu

--- src/components/menu/menu.vue
+++ src/components/menu/menu.vue
@@ -1,19 +1,25 @@
 <script lang="tsx" setup>
-import { ref } from 'vue';
-import type { MenuRef } from '.';
+import { useTemplateRef } from 'vue';
 import { useSiderContextInject } from '../layout/context';
 import type { MenuProps } from './InternalMenu.vue';
 import InternalMenu from './InternalMenu.vue';
 
 defineOptions({ name: 'Menu', inheritAttrs: false, compatConfig: { MODE: 3 } });
 
-defineProps<MenuProps>();
+const { onClick, ...resetProps } = defineProps<MenuProps>();
 
 const selectedKeys = defineModel<any[]>('selectedKeys');
 const openKeys = defineModel('openKeys', { default: [] });
 
-const menuRef = ref<MenuRef>();
+const menuRef = useTemplateRef<HTMLDivElement>('menuRef');
 const context = useSiderContextInject();
+
+function handleClick(e) {
+  if (selectedKeys.value?.length) {
+    selectedKeys.value = [e.key];
+  }
+  onClick?.(e);
+}
 
 defineExpose({
   get menu() {
@@ -27,8 +33,9 @@ defineExpose({
 <template>
   <InternalMenu
     ref="menuRef"
-    v-bind="{ ...$props, ...context }"
+    v-bind="{ ...resetProps, ...context }"
     v-model:selected-keys="selectedKeys"
     v-model:open-keys="openKeys"
+    @click="handleClick"
   />
 </template>
