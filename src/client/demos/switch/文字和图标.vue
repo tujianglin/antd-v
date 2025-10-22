633aad7b377bd068991c4e917749c8a87c03e80a
涂江林 <jianglin.tu@wimisoft.com>
2025-10-22 19:01:46 +0800

    feat: Switch

---
+++ src/client/demos/switch/文字和图标.vue
@@ -0,0 +1,14 @@
+<script lang="tsx" setup>
+import { Space, Switch } from '@/components';
+import { CheckOutlined, CloseOutlined } from '@ant-design/icons-vue';
+import { ref } from 'vue';
+
+const checked = ref(true);
+</script>
+<template>
+  <Space vertical>
+    <Switch checked-children="开启" un-checked-children="关闭" v-model:checked="checked" />
+    <Switch checked-children="1" un-checked-children="0" />
+    <Switch :checked-children="CheckOutlined" :un-checked-children="CloseOutlined" v-model:checked="checked" />
+  </Space>
+</template>
