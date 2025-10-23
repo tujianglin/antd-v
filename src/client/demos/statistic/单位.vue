11277e60ed981060377265239c9d4cc009a9d9a9
涂江林 <jianglin.tu@wimisoft.com>
2025-10-23 22:29:44 +0800

    feat: Statistic

---
+++ src/client/demos/statistic/单位.vue
@@ -0,0 +1,14 @@
+<script lang="tsx" setup>
+import { Col, Row, Statistic } from '@/components';
+import { LikeOutlined } from '@ant-design/icons-vue';
+</script>
+<template>
+  <Row :gutter="16">
+    <Col :span="12">
+      <Statistic title="Feedback" :value="1128" :prefix="LikeOutlined" />
+    </Col>
+    <Col :span="12">
+      <Statistic title="Account Balance (CNY)" :value="93" suffix="/ 100" />
+    </Col>
+  </Row>
+</template>
