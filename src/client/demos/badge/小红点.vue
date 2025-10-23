ddba4279a33ef00fe024333b4f0b8108bb060e54
涂江林 <jianglin.tu@wimisoft.com>
2025-10-23 16:12:41 +0800

    feat: Badge

---
+++ src/client/demos/badge/小红点.vue
@@ -0,0 +1,14 @@
+<script lang="tsx" setup>
+import { Badge, Space } from '@/components';
+import { NotificationOutlined } from '@ant-design/icons-vue';
+</script>
+<template>
+  <Space>
+    <Badge dot>
+      <NotificationOutlined :style="{ fontSize: `${16}px` }" />
+    </Badge>
+    <Badge dot>
+      <a href="#">Link something</a>
+    </Badge>
+  </Space>
+</template>
